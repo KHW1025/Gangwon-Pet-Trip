d0429579f07f8cab40aef6514a08d9d768ef82e6
KHW1025 <119498531+KHW1025@users.noreply.github.com>
2023-05-04 12:35:05 +0900

    Add files via upload

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,16 +1,79 @@
 import { useParams } from "react-router-dom";
 import DetailInfoList from "./DetailInfoList";
 
+import { Link } from "react-router-dom";
+
 function Detail({ list }) {
   let { id } = useParams();
   let detailData = list.filter(
     (item) => item.resultList.contentSeq === Number(id)
   );
 
+  const areaNames = [
+    "인제",
+    "속초",
+    "고성",
+    "양양",
+    "강릉",
+    "동해",
+    "삼척",
+    "양구",
+    "철원",
+    "태백",
+    "화천",
+    "춘천",
+    "홍천",
+    "평창",
+    "횡성",
+    "원주",
+    "정선",
+    "영월",
+  ];
+
+  // const test = detailData[0].resultList.areaName.includes("춘천");
+  // const test2 = areaNames.filter((item) =>
+  //   detailData[0].resultList.areaName.includes(item)
+  // );
+  // const test = areaNames.filter((item) =>
+  //   detailData[0].resultList.areaName.includes(item.resultList.areaName)
+  // );
+
+  // console.log(test);
+  // console.log(test2);
+
   return (
     <>
       <section className="detailBanner">
         <div className="detailBannerInner mw">
+          <ul className="breadCrumbsCon">
+            <li>
+              <Link
+                to={`/city/${areaNames.filter((item) =>
+                  detailData[0].resultList.areaName.includes(item)
+                )}`}
+              >
+                {detailData[0].resultList.areaName}
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={`/city/${areaNames.filter((item) =>
+                  detailData[0].resultList.areaName.includes(item)
+                )}/cate/${
+                  detailData[0].resultList.partName === "식음료"
+                    ? "food"
+                    : detailData[0].resultList.partName === "관광지"
+                    ? "experience"
+                    : detailData[0].resultList.partName === "체험"
+                    ? "experience"
+                    : "hotel"
+                }`}
+              >
+                {detailData[0].resultList.partName}
+              </Link>
+            </li>
+            <li>{detailData[0].resultList.title}</li>
+          </ul>
           <div className="detailInfo">
             <div className="detailImg">
               <img
